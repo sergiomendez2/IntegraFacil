79d16d0cb557593b3cf7de6b417982c0320eb1d4
sergiomendez2 <sergio.mendez2@telusinternational.com>
2023-10-10 12:36:22 -0600

    adding new changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 export const HomePage = () => {
-
     return (
         <>
             <section className="welcome-page">
+                <div className="banner-container">
+                    <img
+                        src="./images/banner.jpg"
+                        alt="Welcome Banner"
+                        className="banner-image"
+                    />
+                </div>
                 <div className="content-overlay">
                     <div className="welcome-content">
                         <h6>Bienvenid@ a</h6>
@@ -12,5 +18,5 @@ export const HomePage = () => {
                 </div>
             </section>
         </>
-    )
-}
+    );
+};
