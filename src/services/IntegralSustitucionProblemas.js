81abeda3f9b12df6034660b08cef0df83307449f
sergiomendez2 <sergio.mendez2@telusinternational.com>
2023-10-10 23:56:20 -0600

    app completed

--- src/services/IntegralSustitucionProblemas.js
+++ src/services/IntegralSustitucionProblemas.js
@@ -10,7 +10,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio1/Paso_1/respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio1/Paso_1/respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 1: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Vuelve la parte mas dificil "u" para poder integrar de la mejor manera.',
         },
         {
             nivel: 'Facil',
@@ -20,7 +20,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio1/Paso_2/respuestas_2/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio1/Paso_2/respuestas_2/Incorrecto_2.png'
             ],
-            consejo: 'Paso 2: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Tu parte mas dificil sustituyela por "u" luego opera para tener todo en terminis de una solo variable y la integracion sea mas facil y eficiente.',
         },
         {
             nivel: 'Facil',
@@ -30,7 +30,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio1/Paso_3/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio1/Paso_3/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 3: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Recuerda al integrar al exponente se le suma uno, cuando tengas una fraccion como denominador puedes usar la ley del sandwich para que te quede una fraccion simple.',
         },
         {
             nivel: 'Facil',
@@ -40,7 +40,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio1/Paso_4/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio1/Paso_4/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 4: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Los exponentes al integrar se les suma uno y esto no cambia al efectuar las sustituciones correspondientes, verifica tus exponentes y tus signos.',
         },
     ],
 
@@ -53,7 +53,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio2/Paso_1/respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio2/Paso_1/respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 1: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al sustituir se colocan los signos que se tiene en la funcion, asi mismo al efectuar las operaciones de la sustitucion, se deriva, no se integra.',
         },
         {
             nivel: 'Facil',
@@ -63,7 +63,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio2/Paso_2/Respuestas/Incorrecta_1.png',
                 './images/sustitucion/facil/ejercicio2/Paso_2/Respuestas/Incorrecta_2.png'
             ],
-            consejo: 'Paso 2: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Para tener una integral mas facil de resolver es recomendable sacar fuera de la integral la constante esta sale de la misma forma con la que se encuentra dentro de la integral sin afectar a su numerados y deniminador.',
         },
         {
             nivel: 'Facil',
@@ -73,7 +73,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio2/Paso_3/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio2/Paso_3/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 3: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al efectuar la integracion al exponente "n" se le suma uno, esto mismo se efectua como denominador quedando una fraccion, en varios casos puede efectur cancelaciones.',
         },
         {
             nivel: 'Facil',
@@ -83,7 +83,7 @@ const problems = [
                 './images/sustitucion/facil/ejercicio2/Paso_4/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/facil/ejercicio2/Paso_4/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 4: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Despues de las integracios puedes hacer una multiplicacion recuerda que esto lo efectuas lineal, al sustituir la variable "u" el exponente afecta a todo el ternino.',
         },
     ],
 
@@ -97,7 +97,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_1/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_1/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 1: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Despues de hacer las sustituciones puedes corroborar que al derivar te queden terminos que aparecen en la integral original, puedes sustituirlas para hacer una integral mas facil de resolver.',
         },
         {
             nivel: 'Intermedio',
@@ -107,7 +107,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_2/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_2/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 2: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Las cosntantes las puedes sacar la integral para despues poder integrar de la forma mas facil, ten presente que los exponentes no afectan aun a los procedimientos.',
         },
         {
             nivel: 'Intermedio',
@@ -117,7 +117,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_3/Respuestas/Incorrecta_2.png',
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_3/Respuestas/Incorrecto_1.png'
             ],
-            consejo: 'Paso 3: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al integrar el exponente se le suma uno, esto mismo lo tenes que operar en el deniminador, luego de haber echo los pasos anteriores, la constate que esta fuera de la integral la debes de multiplicar por el resultado que te dio al resilver la integral.',
         },
         {
             nivel: 'Intermedio',
@@ -127,7 +127,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_4/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_1/Paso_4/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 4: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al finalizar sustituye las varibles que tienes, estas colocalas con el mismo signo que tenian en la funcion original mas la constante de integracion que es "c",\n',
         },
     ],
 
@@ -140,7 +140,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_1/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_1/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 1: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Para hacer una integral mas facil de resolver dejala lineal, al pasar un denominar como numerador el exponente se coloca negativo.',
         },
         {
             nivel: 'Intermedio',
@@ -150,7 +150,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_2/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_2/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 2: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Haz "u" la parte mas complicada sin importar el signo del exponente, coloca los signos correctamente esto puede ocacionarte una respuseta erronea.',
         },
         {
             nivel: 'Intermedio',
@@ -160,7 +160,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_3/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_3/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 3: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al hacer las sustitucion dentro de la funcion el signo del exponente no lo afecta, ten cuidado como planteas la integral.',
         },
         {
             nivel: 'Intermedio',
@@ -170,7 +170,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_4/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_4/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 4: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al efectuar una multiplicacion los exponente se suman, cuando tienes una funcion que contiene sumas puedes separarlas por integrales en esta no se afectan por el signo del exponente.',
         },
         {
             nivel: 'Intermedio',
@@ -180,7 +180,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_5/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_5/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 5: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Los expoentes se ven afecados al realizar la integracion, se le suma uno, esto tambien ocurre en la parte del denominador se efectua la suma del exponente mas uno, recuerda apoyarte de la ley de sandwich para efectuar estas fracciones, revisa los signos de tu funcion.',
         },
         {
             nivel: 'Intermedio',
@@ -190,7 +190,7 @@ const problems = [
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_6/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/Intermedio/Ejercicio_2/Paso_6/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 6: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Al sustituir el resultado de la integracion, revisa los signos de la sustitucion que sean los corretos, puedes apoyarte del procedimiento por si no te recuerdas con hiciste las sustitucion inicial.',
         }
     ],
 
@@ -204,7 +204,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_1/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_1/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 1: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Si elevas al cuadro de un lado lo debes de hacer del otro lado de la igualdad para conservarlo.',
         },
         {
             nivel: 'Dificil',
@@ -214,7 +214,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_2/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_2/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 2: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Recuerda sustituir la "u" dentro de la funcion, no efectues multiplicacion ya que tienes una raiz, recisa los signos de tu integral, recuerda que las constantes las puedes sacar de la integral.',
         },
         {
             nivel: 'Dificil',
@@ -224,7 +224,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_3/Respuestas/Incorrecto.png',
                 './images/sustitucion/dificil/Paso_3/Respuestas/Incorrecto_1.png'
             ],
-            consejo: 'Paso 3: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Haz "u" la parte mas dificil, puedes utilizar otra varible no especificamente tiene que ser "u", revisa colocar la usustitucion con sus signos correctos.',
         },
         {
             nivel: 'Dificil',
@@ -234,7 +234,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_4/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_4/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 4: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Sustituye la integral con la variable que haz ezcogido, no sustituyas por las variables que se escogio la primera vez.',
         },
         {
             nivel: 'Dificil',
@@ -244,7 +244,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_5/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_5/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 5: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Recuerda que la raiz cuadrada la puedes expresar como fraccion y al multiplicar este se suman sus exponentes, no lo trabajes como raiz, te complicaras mas.',
         },
         {
             nivel: 'Dificil',
@@ -254,7 +254,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_6/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_6/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 6: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Recuerda que al integrar es "n" mas uno, y esto tambien se efectua en el deniminador, no te olvides de la ley del sandwich te sera muy util para las fracciones.',
         },
         {
             nivel: 'Dificil',
@@ -264,7 +264,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_7/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_7/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 7: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Revisa tus exponentes estos deben de estar correctamente, puedes apoyarte del proceso para verificar que estas yendo por el buen camino.',
         },
         {
             nivel: 'Dificil',
@@ -274,7 +274,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_8/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_8/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 8: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'No te dejes engañar aunque los exponentes pueden se iguales, revisa tus signos dentro de tus parentesis algo puedes tener mal, te recomiendo que verifiques los exponentes de las demas variantes.',
         },
         {
             nivel: 'Dificil',
@@ -284,7 +284,7 @@ const problems = [
                 './images/sustitucion/dificil/Paso_9/Respuestas/Incorrecto_1.png',
                 './images/sustitucion/dificil/Paso_9/Respuestas/Incorrecto_2.png'
             ],
-            consejo: 'Paso 8: Recuerda revisar cuidadosamente el problema y calcular las opciones posibles en este paso.',
+            consejo: 'Cambia las variables por las sutituciones que se hayan efectuado, recuerda se hicieron dos sustituciones, primero sustituye por la segunda sustitucion y despues sustituye por la primera, te recomendamos que veas tus signos.',
         }
     ],
 ];
